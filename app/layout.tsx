4992b82f2a40cbbf3cc0d7f43e58685d61bdc806
jason <jason.huang@megadatatech.com>
2025-06-17 18:11:54 +0800

    fix: prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,38 +10,38 @@ import { fontSans } from '@/config/fonts'
 import { Navbar } from '@/components/navbar'
 
 export const metadata: Metadata = {
-    title: {
-        default: siteConfig.name,
-        template: `%s - ${siteConfig.name}`,
-    },
-    description: siteConfig.description,
-    icons: {
-        icon: '/favicon.ico',
-    },
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export const viewport: Viewport = {
-    themeColor: [
-        { media: '(prefers-color-scheme: light)', color: 'white' },
-        { media: '(prefers-color-scheme: dark)', color: 'black' },
-    ],
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
 }
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    return (
-        <html suppressHydrationWarning lang="en">
-            <head />
-            <body
-                className={clsx(
-                    'min-h-screen bg-background font-sans antialiased',
-                    fontSans.variable
-                )}
-            >
-                {/* inset-0 - 填满整个视口
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        {/* inset-0 - 填满整个视口
 
                 bg-cover - 覆盖整个区域
  
@@ -52,16 +52,14 @@ export default function RootLayout({
                 opacity-50 - 50%透明度
 
                 z-0 - 确保在其他内容之下 */}
-                {/* <div className="fixed inset-0 bg-[url('/image/bg.png')] bg-cover bg-center bg-no-repeat bg-fixed opacity-50 z-0"></div> */}
-                <Providers
-                    themeProps={{ attribute: 'class', defaultTheme: 'light' }}
-                >
-                    <div className="relative flex flex-col h-screen">
-                        <Navbar />
-                        <main className="container mx-auto max-w-7xl px-6 flex-grow">
-                            {children}
-                        </main>
-                        {/* <footer className="w-full flex items-center justify-center py-3">
+        {/* <div className="fixed inset-0 bg-[url('/image/bg.png')] bg-cover bg-center bg-no-repeat bg-fixed opacity-50 z-0"></div> */}
+        <Providers themeProps={{ attribute: 'class', defaultTheme: 'light' }}>
+          <div className="relative flex flex-col h-screen">
+            <Navbar />
+            <main className="container mx-auto max-w-7xl px-6 flex-grow">
+              {children}
+            </main>
+            {/* <footer className="w-full flex items-center justify-center py-3">
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
@@ -72,9 +70,9 @@ export default function RootLayout({
                 <p className="text-primary">HeroUI</p>
               </Link>
             </footer> */}
-                    </div>
-                </Providers>
-            </body>
-        </html>
-    )
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,109 +8,88 @@ import HeroImage from '@/components/hero-image'
 const aboutMe = `I am a front-end developer who loves traveling, music, and technology.`
 
 export default function Home() {
-    return (
-        <div>
-            <div className="relative flex justify-end">
-                {/* <Image
+  return (
+    <div>
+      <div className="relative flex justify-end">
+        {/* <Image
                     isBlurred
                     alt="HeroUI Album Cover"
                     className="m-5"
                     src="/image/my-avatar.jpg"
                     width={240}
                 /> */}
-                <div className="absolute left-0 top-52">
-                    <p className="text-4xl font-bold">
-                        Hi 👋🏻! This is Geeki's blog
-                    </p>
-                    <TextGenerateEffect words={aboutMe} />
-                    <div className="flex mt-5 gap-4">
-                        <InteractiveHoverButton>
-                            CV (English)
-                        </InteractiveHoverButton>
-                        <InteractiveHoverButton>
-                            简历（中文）
-                        </InteractiveHoverButton>
-                    </div>
-                </div>
-                <div className="relative w-3/4">
-                    <HeroImage />
-                </div>
-            </div>
-
-            <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
-                <OrbitingCircles>
-                    <Image
-                        src="/icons/React.svg"
-                        alt="React Icon"
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        src="/icons/Vue.svg"
-                        alt="Vue Icon"
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        src="/icons/Angular.svg"
-                        alt="Angular Icon"
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        src="/icons/CSS3.svg"
-                        alt="CSS3 Icon"
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        src="/icons/HTML5.svg"
-                        alt="HTML5 Icon"
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        src="/icons/Typescript.svg"
-                        alt="Typescript Icon"
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        src="/icons/Javascript.svg"
-                        alt="Javascript Icon"
-                        width={50}
-                        height={50}
-                    />
-                </OrbitingCircles>
-                <OrbitingCircles radius={90} reverse>
-                    <Image
-                        src="/icons/Android.svg"
-                        alt="Android Icon"
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        src="/icons/Ios.svg"
-                        alt="Ios Icon"
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        src="/icons/Github.svg"
-                        alt="Github Icon"
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        src="/icons/Wireshark.svg"
-                        alt="Wireshark Icon"
-                        width={50}
-                        height={50}
-                    />
-                </OrbitingCircles>
-            </div>
-
-            <div className="flex flex-col gap-10"></div>
+        <div className="absolute left-0 top-52">
+          <p className="text-4xl font-bold">Hi 👋🏻! This is Geeki&apos;s blog</p>
+          <TextGenerateEffect words={aboutMe} />
+          <div className="flex mt-5 gap-4">
+            <InteractiveHoverButton>CV (English)</InteractiveHoverButton>
+            <InteractiveHoverButton>简历（中文）</InteractiveHoverButton>
+          </div>
         </div>
-    )
+        <div className="relative w-3/4">
+          <HeroImage />
+        </div>
+      </div>
+
+      <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
+        <OrbitingCircles>
+          <Image
+            src="/icons/React.svg"
+            alt="React Icon"
+            width={50}
+            height={50}
+          />
+          <Image src="/icons/Vue.svg" alt="Vue Icon" width={50} height={50} />
+          <Image
+            src="/icons/Angular.svg"
+            alt="Angular Icon"
+            width={50}
+            height={50}
+          />
+          <Image src="/icons/CSS3.svg" alt="CSS3 Icon" width={50} height={50} />
+          <Image
+            src="/icons/HTML5.svg"
+            alt="HTML5 Icon"
+            width={50}
+            height={50}
+          />
+          <Image
+            src="/icons/Typescript.svg"
+            alt="Typescript Icon"
+            width={50}
+            height={50}
+          />
+          <Image
+            src="/icons/Javascript.svg"
+            alt="Javascript Icon"
+            width={50}
+            height={50}
+          />
+        </OrbitingCircles>
+        <OrbitingCircles radius={90} reverse>
+          <Image
+            src="/icons/Android.svg"
+            alt="Android Icon"
+            width={50}
+            height={50}
+          />
+          <Image src="/icons/Ios.svg" alt="Ios Icon" width={50} height={50} />
+          <Image
+            src="/icons/Github.svg"
+            alt="Github Icon"
+            width={50}
+            height={50}
+          />
+          <Image
+            src="/icons/Wireshark.svg"
+            alt="Wireshark Icon"
+            width={50}
+            height={50}
+          />
+        </OrbitingCircles>
+      </div>
+
+      <div className="flex flex-col gap-10"></div>
+    </div>
+  )
 }
